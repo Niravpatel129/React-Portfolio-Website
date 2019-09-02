a79c02df56802eed735e4bf23d7a163bf2d27ddb
Niravpatel129 <niravpatelp129@gmail.com>
2019-09-02 13:43:07 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -5,15 +5,26 @@ import FullScreenMedia from "./components/FullScreenMedia/FullScreenMedia";
 import QuoteSection from "./components/QuoteSection/QuoteSection";
 import VideoSection from "./components/VideoSection/VideoSection";
 
-function App() {
+function isMobileDevice() {
   return (
-    <div className="App">
-      <Toolbar />
-      <FullScreenMedia />
-      <QuoteSection />
-      <VideoSection />
-    </div>
+    typeof window.orientation !== "undefined" ||
+    navigator.userAgent.indexOf("IEMobile") !== -1
   );
+}
+function App() {
+  if (!isMobileDevice) {
+    console.log("mo");
+    return <h1>Site is not made for mobile yet! Check back soon :)</h1>;
+  } else {
+    return (
+      <div className="App">
+        <Toolbar />
+        <FullScreenMedia />
+        <QuoteSection />
+        <VideoSection />
+      </div>
+    );
+  }
 }
 
 export default App;
