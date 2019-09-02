eb737ad306fbde1c56208efd8fbca8372ae079d9
Niravpatel129 <niravpatelp129@gmail.com>
2019-09-01 22:57:56 -0400

    video function added

--- src/components/VideoSection/VideoSection.js
+++ src/components/VideoSection/VideoSection.js
@@ -6,14 +6,42 @@ class VideoSection extends Component {
   constructor(props) {
     super(props);
     this.state = { videoAutoPlaying: "False" };
+    this.playButtonClassNames = "playpause";
   }
+
+  playVideo() {
+    console.log(this.refs.vidButtonRef);
+    if (this.state.videoAutoPlaying === "True") {
+      this.setState({ videoAutoPlaying: "False" });
+      console.log("pause");
+      this.playButtonClassNames = "playpause m-fadeIn";
+
+      this.refs.vidRef.pause();
+    } else {
+      this.setState({ videoAutoPlaying: "True" });
+      this.playButtonClassNames = "playpause m-fadeOut";
+
+      console.log("play");
+      this.refs.vidRef.play();
+    }
+  }
+
   render() {
     return (
-      <div className="VideoSection">
-        <video width="722" controls>
+      <div className="wrapper">
+        <video
+          loop={true}
+          className="video"
+          ref="vidRef"
+          onClick={this.playVideo.bind(this)}
+        >
           <source src={video} type="video/mp4" />
-          Your browser does not support HTML5 video.
         </video>
+        <div
+          className={this.playButtonClassNames}
+          ref="vidButtonRef"
+          onClick={this.playVideo.bind(this)}
+        ></div>
       </div>
     );
   }
