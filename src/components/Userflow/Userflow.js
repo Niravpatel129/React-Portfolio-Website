8c7f54f2cf89597a03297d4f985531372c986b54
Niravpatel129 <niravpatelp129@gmail.com>
2019-09-06 10:48:39 -0400

    removed poc

---
+++ src/components/Userflow/Userflow.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import "./Userflow.scss";
+import image from "./image.png";
+
+class Userflow extends Component {
+  render() {
+    return <img src={image}></img>;
+  }
+}
+
+export default Userflow;
