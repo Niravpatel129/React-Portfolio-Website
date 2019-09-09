4856432c603ad01267a6eedca6140184529c829c
niravpatel129 <niravpatelp129@gmail.com>
2019-09-09 12:36:12 -0400

    minor changes

--- src/components/InformationComponent/InformationComponent.js
+++ src/components/InformationComponent/InformationComponent.js
@@ -5,6 +5,26 @@ import font from "../fonts/MaisonNeue-Light.otf";
 
 console.log(font);
 class InformationComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { videoAutoPlaying: "False" };
+    this.playButtonClassNames = "newplaybutton";
+  }
+
+  playVideo() {
+    if (this.state.videoAutoPlaying === "True") {
+      this.setState({ videoAutoPlaying: "False" });
+      this.playButtonClassNames = "newplaybutton m-fadeIn";
+
+      this.refs.vidRef.pause();
+    } else {
+      this.setState({ videoAutoPlaying: "True" });
+      this.playButtonClassNames = "newplaybutton m-fadeOut";
+
+      this.refs.vidRef.play();
+    }
+  }
+
   render() {
     return (
       <div className="InformationComponent">
@@ -30,10 +50,19 @@ class InformationComponent extends Component {
             </p>
           </div>
           <div className="videoBoxContainer">
-            <video loop={true} className="videoBox" ref="vidRef">
+            <video
+              loop={true}
+              className="videoBoxComponent"
+              ref="vidRef"
+              onClick={this.playVideo.bind(this)}
+            >
               <source src={video} type="video/mp4" />
             </video>
-            <div className={this.playButtonClassNames} ref="vidButtonRef"></div>
+            {/* <div
+              className={this.playButtonClassNames}
+              ref="vidButtonRef"
+              onClick={this.playVideo.bind(this)}
+            ></div> */}
           </div>
         </div>
         <div className="bottomblock">
