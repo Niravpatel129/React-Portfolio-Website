4eda9eef5413df85a22f459e8e93123339235ac0
niravpatel129 <niravpatelp129@gmail.com>
2019-09-12 16:47:32 -0400

    changes

--- src/components/ImageBoxes/ImageBoxes.js
+++ src/components/ImageBoxes/ImageBoxes.js
@@ -21,7 +21,7 @@ class ImageBoxes extends Component {
           <h4>
             Character <br></br>Design
           </h4>
-          <div className="bottom">
+          <div className="top">
             <img src={Wolf1} alt={Wolf1}></img>
             <img src={Wolf2} alt={Wolf2}></img>
             <img src={Pig1} alt={Pig1}></img>
@@ -41,6 +41,20 @@ class ImageBoxes extends Component {
             <img src={Land2} alt={Pig2}></img>
           </div>
         </div>
+        <h4 className="conclusion">
+          The <br></br>Conclusion
+        </h4>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged.<br></br> <br></br>It was popularised
+          in the 1960s with the release of Letraset sheets containing Lorem
+          Ipsum passages, and more recently with desktop publishing software
+          like Aldus PageMaker including versions of Lorem Ipsum.
+        </p>
       </div>
     );
   }
