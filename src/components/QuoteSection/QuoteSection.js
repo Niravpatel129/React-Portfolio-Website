a60744ca62a05b715d93258844b526657a7433cf
niravpatel129 <niravpatelp129@gmail.com>
2019-09-22 00:09:28 -0400

    changes

--- src/components/QuoteSection/QuoteSection.js
+++ src/components/QuoteSection/QuoteSection.js
@@ -11,21 +11,21 @@ class QuoteSection extends Component {
       <div className="QuoteSection">
         <h1>StoryTale</h1>
         <div className="typeboxes">
-          <div className="typebox">
+          <div className="typebox1">
             <h3>TYPE</h3>
             <p>
               UI/UX, Product, Interaction, Website, Augmented Reality, Voice
               Recognition
             </p>
           </div>
-          <div className="typebox">
+          <div className="typebox2">
             <h3>TOOLS</h3>
             <p>
               Sketch, Cinema4D, AfterEffects, Mixamo, 8thWall, Voice Recogition
               API
             </p>
           </div>
-          <div className="typebox">
+          <div className="typebox3">
             <h3>TIME</h3>
             <p>June 2019 - August 2019 ( 3 months )</p>
           </div>
@@ -39,21 +39,21 @@ class QuoteSection extends Component {
           </h2>
         </div>
         <div className="typeboxes">
-          <div className="typebox">
+          <div className="typebox1">
             <h3>1. OPPROTUNITIES</h3>
             <p>
               Building an experience that blends analog and digital in a clever
               and meaningful way.
             </p>
           </div>
-          <div className="typebox">
+          <div className="typebox2">
             <h3>2. ENGAGMENT</h3>
             <p>
               Create a storytelling experience that allows families to have a
               deeper connection with stories by engaging more of their senses.
             </p>
           </div>
-          <div className="typebox">
+          <div className="typebox3">
             <h3>3. COLLABORATION</h3>
             <p>
               Generate a product that encourages families to better connect with
