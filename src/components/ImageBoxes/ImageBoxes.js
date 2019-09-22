452211d5ea3ab5738cf98ee472a55ee764371959
niravpatel129 <niravpatelp129@gmail.com>
2019-09-21 23:48:59 -0400

    fixed

--- src/components/ImageBoxes/ImageBoxes.js
+++ src/components/ImageBoxes/ImageBoxes.js
@@ -45,15 +45,10 @@ class ImageBoxes extends Component {
           The <br></br>Conclusion
         </h4>
         <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged.<br></br> <br></br>It was popularised
-          in the 1960s with the release of Letraset sheets containing Lorem
-          Ipsum passages, and more recently with desktop publishing software
-          like Aldus PageMaker including versions of Lorem Ipsum.
+          Parents and their kids are more digitally oriented than ever. As shown
+          with Storytale, storytelling can be more collaborative and engaging.
+          <br></br> <br></br>Instead of replacing the physical experience, we
+          wanted to see this as an opprotunity and enhance it.
         </p>
       </div>
     );
